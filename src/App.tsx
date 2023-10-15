a2fa2cc2af8c8b25a85aedf544741d75df846ac8
Dc Valerian <Dc-Valerian>
2023-10-15 01:02:55 +0100

    added

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
 // App.tsx
 import React from "react";
 import ContactList from "./pages/Contact/ContactList";
-import { Contact } from "./pages/Contact/Contact";
-
-const contacts: Contact[] = [
-  { id: 1, name: "John Doe", number: "123-456-7890" },
-  { id: 2, name: "Jane Smith", number: "987-654-3210" },
-  { id: 3, name: "Amidat", number: "987-654-3210" },
-  { id: 4, name: "Austin", number: "987-654-3210" },
-  { id: 5, name: "Valerian", number: "987-654-3210" },
-  // Add more contacts
-];
 
 const App: React.FC = () => {
   return (
     <div>
       <h1>Contact List</h1>
-      <ContactList contacts={contacts} />
+      <ContactList />
     </div>
   );
 };
--- src/pages/Contact/ContactList.tsx
+++ src/pages/Contact/ContactList.tsx
@@ -1,15 +1,42 @@
-// ContactList.tsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Contact } from "./Contact";
 
-interface ContactListProps {
-  contacts: Contact[];
-}
-
-const ContactList: React.FC<ContactListProps> = ({ contacts }) => {
+const ContactList: React.FC = () => {
   const [query, setQuery] = useState("");
   const [searchResults, setSearchResults] = useState<Contact[]>([]);
   const [notFound, setNotFound] = useState(false);
+  const [showAllContacts, setShowAllContacts] = useState(false);
+  const [newContact, setNewContact] = useState<Contact>({
+    id: 0,
+    name: "",
+    number: "",
+  });
+  const [successMessage, setSuccessMessage] = useState("");
+  const [contacts, setContacts] = useState<Contact[]>([]);
+  const [favorites, setFavorites] = useState<Contact[]>([]);
+
+  useEffect(() => {
+    const storedContacts = localStorage.getItem('contacts');
+
+    if (storedContacts) {
+      setContacts(JSON.parse(storedContacts));
+    } else {
+      const defaultContacts: Contact[] = [
+        { id: 1, name: 'John Doe', number: '123-456-7890' },
+        { id: 2, name: 'Jane Smith', number: '987-654-3210' }
+      ];
+      setContacts(defaultContacts);
+      localStorage.setItem('contacts', JSON.stringify(defaultContacts));
+    }
+  }, []);
+
+  // Load contacts from the JSON file on component mount
+  useEffect(() => {
+    fetch("/path-to-your-json-file/contacts.json") // Replace with the actual path
+      .then((response) => response.json())
+      .then((data) => setContacts(data))
+      .catch((error) => console.error("Error loading contacts:", error));
+  }, []);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const searchTerm = event.target.value;
@@ -35,6 +62,86 @@ const ContactList: React.FC<ContactListProps> = ({ contacts }) => {
     }
   };
 
+  const handleShowAllContacts = () => {
+    if (showAllContacts) {
+      setSearchResults([]); // Clear the results
+    } else {
+      setSearchResults(contacts); // Set the results to all contacts
+    }
+    setNotFound(false); // Reset the "not found" state
+    setShowAllContacts(!showAllContacts); // Toggle the showAllContacts state
+  };
+
+  const handleSaveContact = () => {
+    if (newContact.name && newContact.number) {
+      if (newContact.number.length >= 11) {
+        // Check if the name has already been saved
+        if (contacts.some(contact => contact.name === newContact.name)) {
+          setSuccessMessage('Name has already been saved');
+        } else {
+          // Create a copy of the existing contacts and add the new contact
+          const updatedContacts = [...contacts, newContact];
+  
+          // Update the state with the new contacts
+          setContacts(updatedContacts);
+  
+          // Clear the new contact fields after saving
+          setNewContact({ id: 0, name: '', number: '' });
+  
+          // Save the updated contacts to local storage
+          localStorage.setItem('contacts', JSON.stringify(updatedContacts));
+  
+          setSuccessMessage('Contact has been added successfully');
+        }
+      } else {
+        setSuccessMessage('Number is not complete (at least 11 characters required)');
+      }
+    }
+  };
+
+
+// const handleSaveContact = () => {
+//     if (newContact.name && newContact.number) {
+//       if (newContact.number.length >= 11) {
+//         if (contacts.some(contact => contact.name === newContact.name)) {
+//           setSuccessMessage('Name has already been saved');
+//         } else {
+//           const updatedContacts = [...contacts, newContact];
+//           setContacts(updatedContacts);
+//           setNewContact({ id: 0, name: '', number: '' });
+//           localStorage.setItem('contacts', JSON.stringify(updatedContacts));
+//           setSuccessMessage('Contact has been added successfully');
+//         }
+//       } else {
+//         setSuccessMessage('Number is not complete (at least 11 characters required)');
+//       }
+//     }
+//   };
+
+  
+  const handleDeleteContact = (id: number) => {
+    const updatedContacts = contacts.filter(contact => contact.id !== id);
+    setContacts(updatedContacts);
+    localStorage.setItem('contacts', JSON.stringify(updatedContacts));
+  };
+  
+
+  const handleToggleFavorite = (id: number) => {
+    const updatedFavorites = [...favorites];
+    const contact = contacts.find(contact => contact.id === id);
+    if (contact) {
+      const existingIndex = favorites.findIndex(fav => fav.id === id);
+      if (existingIndex !== -1) {
+        // Remove from favorites
+        updatedFavorites.splice(existingIndex, 1);
+      } else {
+        // Add to favorites
+        updatedFavorites.push(contact);
+      }
+      setFavorites(updatedFavorites);
+    }
+  };
+
   return (
     <div>
       <input
@@ -43,6 +150,9 @@ const ContactList: React.FC<ContactListProps> = ({ contacts }) => {
         value={query}
         onChange={handleInputChange}
       />
+      <button onClick={handleShowAllContacts}>
+        {showAllContacts ? "Hide All Contacts" : "Show All Contacts"}
+      </button>
       {notFound ? (
         <p>Contact not found</p>
       ) : (
@@ -54,6 +164,53 @@ const ContactList: React.FC<ContactListProps> = ({ contacts }) => {
           ))}
         </ul>
       )}
+
+      <h2>Add New Contact</h2>
+      <input
+        type="text"
+        placeholder="Name"
+        value={newContact.name}
+        onChange={(e) => setNewContact({ ...newContact, name: e.target.value })}
+      />
+      <input
+        type="text"
+        placeholder="Number"
+        value={newContact.number}
+        onChange={(e) =>
+          setNewContact({ ...newContact, number: e.target.value })
+        }
+      />
+      <button onClick={handleSaveContact}>Save Contact</button>
+
+      {successMessage && <p>{successMessage}</p>}
+
+      <h2>Contact List</h2>
+      <ul>
+        {contacts.map(contact => (
+          <li key={contact.id}>
+            {contact.name} - {contact.number}
+            <button onClick={() => handleDeleteContact(contact.id)}>Delete</button>
+            <span
+              role="img"
+              aria-label="Favorite"
+              onClick={() => handleToggleFavorite(contact.id)}
+              style={{
+                cursor: 'pointer',
+                backgroundColor: favorites.some(fav => fav.id === contact.id) ? 'red' : 'transparent'
+              }}
+            >
+              ❤️
+            </span>
+          </li>
+        ))}
+      </ul>
+
+      <h2>Favorites</h2>
+      <ul>
+        {favorites.map(favorite => (
+          <li key={favorite.id}>{favorite.name} - {favorite.number}</li>
+        ))}
+      </ul>
     </div>
   );
 };
